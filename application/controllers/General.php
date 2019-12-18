2a16381be5673a4a930fd15ec990c5c52021faa4
titoanugerah <titoanugerah@student.undip.ac.id>
2019-12-18 18:17:17 +0700

    Google Login done

--- application/controllers/General.php
+++ application/controllers/General.php
@@ -13,8 +13,38 @@ class General extends CI_Controller
     error_reporting(1);
   }
 
-  public function index(){
-    $this->load->view('general/home');
+  public function index()
+  {
+//    $this->load->view('general/home');
+    $this->login();
+  }
+
+  public function login()
+  {
+    require_once 'vendor/autoload.php';
+  //  $redirect_uri = 'https://google.com';
+    $client = new Google_Client();
+    $client->setAuthConfig('assets/client_credentials.json');
+    //$client->setRedirectUri($redirect_uri);
+    $client->addScope("email");
+    $client->addScope("profile");
+
+    if (isset($_GET['code']))
+    {
+//      var_dump(isset($_GET['code']));die;
+      $token = $client->fetchAccessTokenWithAuthCode($this->input->get('code'));
+      $client->setAccessToken($token['access_token']);
+
+      $google_oauth = new Google_Service_Oauth2($client);
+      $google_account_info = $google_oauth->userinfo->get();
+      $email = $google_account_info->email;
+      $name = $google_account_info->name;
+      echo $name.' -- '.$email;
+    }
+    else
+    {
+      echo '<a href="'.$client->createAuthUrl().'">Google Login </a>';
+    }
   }
 }
 
