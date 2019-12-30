09a2dfb3cbe2ecd3616bc9dcac7a6167f89c30d0
titoanugerah <titoanugerah@student.undip.ac.id>
2019-12-30 22:23:37 +0700

    savepoint 1

--- application/views/management/AccountManagement.php
+++ application/views/management/AccountManagement.php
@@ -6,6 +6,7 @@
         <h5 class="text-white op-7 mb-2"> Halaman panel pengelolaan Akun</h5>
       </div>
       <div class="ml-md-auto py-2 py-md-0">
+        <button type="button" data-toggle="modal" data-target="#addManagement" class="btn btn-round btn-success btn-border" >Tambah Karyawan</button>
         <a href="<?php echo base_url('accountManagement/'.((int)$this->session->userdata['page']-1)); ?>" class="btn btn-success btn-round" <?php if($this->session->userdata['page']==0){echo 'hidden';}?>>Sebelumnya</a>
         <a href="<?php echo base_url('accountManagement/'.((int)$this->session->userdata['page']+1)); ?>" class="btn btn-success btn-round">Selanjutnya</a>
       </div>
@@ -20,7 +21,7 @@
     </div>
   </div>
 </div>
-<div class="card">
+
   <div class="tab-content mt-2 mb-3" >
     <div class="tab-pane fade show active" id="tab1" role="tabpanel" >
       <div class="card-body row">
@@ -63,7 +64,6 @@
       </div>
     </div>
   </div>
-</div>
 
 <div class="modal fade" id="detailAccount" role="dialog">
   <div class="modal-dialog modal-lg">
@@ -200,6 +200,73 @@
     </div>
   </div>
 </div>
+
+<div class="modal fade" id="addManagement" role="dialog">
+  <div class="modal-dialog">
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <center>
+          <h4>Tambah Akun</h4>
+        </center>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+      <form role="form" method="post">
+        <div class="modal-body">
+          <ul class="wizard-menu nav nav-pills nav-primary">
+            <li class="step" style="width: 50%;">
+              <a class="nav-link active" href="#dataManagement" data-toggle="tab" aria-expanded="true"><i class="fa fa-user mr-0"></i> Detail Karyawan</a>
+            </li>
+            <li class="step" style="width: 50%;">
+              <a class="nav-link" href="#addPrivilegesList" data-toggle="tab"><i class="fa fa-file mr-2"></i> Daftar Hak Akses</a>
+            </li>
+          </ul>
+          <div class="tab-content">
+            <div class="tab-pane active" id="dataManagement">
+              <div class="row">
+                <div class="form-group col-6 col-md-12">
+                  <label>Email</label>
+                  <input type="email" class="form-control" id="addEmailManagement" placeholder="silahkan masukan email disini" required>
+                </div>
+            </div>
+          </div>
+          <div class="tab-pane" id="addPrivilegesList">
+            <br><br>
+            <div class="selectgroup selectgroup-pills">
+							<label class="selectgroup-item">
+								<input type="checkbox" id="addHome"  value="1" class="selectgroup-input" checked disabled>
+								<span class="selectgroup-button">Halaman Depan</span>
+							</label>
+							<label class="selectgroup-item">
+								<input type="checkbox" id="addAccountManagement" value="1" class="selectgroup-input">
+								<span class="selectgroup-button">Manajemen Akun</span>
+							</label>
+							<label class="selectgroup-item">
+								<input type="checkbox" id="addStockManagement" value="1" class="selectgroup-input">
+								<span class="selectgroup-button">Manajemen Stok</span>
+							</label>
+              <br><br>
+							<label class="selectgroup-item">
+								<input type="checkbox" id="addSalesManagement" value="1" class="selectgroup-input">
+								<span class="selectgroup-button">Manajemen Penjualan</span>
+							</label>
+							<label class="selectgroup-item">
+								<input type="checkbox" id="addReporting" value="1" class="selectgroup-input">
+								<span class="selectgroup-button">Laporan </span>
+							</label>
+						</div>
+          </div>
+        </div>
+      </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary" onclick="addManagement()">Simpan</button>
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Kembali</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+
 <script type="text/javascript">
   function GetDetailCustomer(id) {
     $.ajax({
@@ -262,10 +329,26 @@
     return result.toString();
   }
 
+
   function UpdateAccountManagement() {
   var summary =  parseInt((checker('reporting') + checker('salesManagement') + checker('stockManagement') + checker('accountManagement') + checker('home')),2);
   var urls = "<?php echo base_url() ?>updateAccountManagement?Id="+$("#idManagement").val()+"&Privilleges="+summary
   $("#detailAccountManagement").modal('hide');
+    $.ajax({
+        type: "POST",
+        dataType : "JSON",
+        url: urls ,
+        success: function(result) {
+         notify('fa fa-user', result.title, result.message, result.type);
+        },
+        error: function(result) {
+            console.log(result);
+          alert('err');
+        }
+    });
+  }
+
+  function addAccountManagement() {
     $.ajax({
         type: "POST",
         dataType : "JSON",
